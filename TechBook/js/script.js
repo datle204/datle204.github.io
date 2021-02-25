8d1090f8b973f61b3156917e0e649c0393409c1e
dat <datle20497@gmail.com>
2021-02-26 06:00:10 +0700

    update

---
+++ TechBook/js/script.js
@@ -0,0 +1,27 @@
+// CUSTOMER COMMENT CAROUSEL
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#card-slider", {
+    type: "loop",
+    cover: true,
+    autoplay: true,
+    interval: 4000,
+    perPage: 3,
+    rewind: true,
+    pagination: false,
+    breakpoints: {
+      900: {
+        perPage: 1,
+      },
+      1200: {
+        perPage: 2,
+      },
+    },
+  }).mount();
+});
+
+// EVENT CLICK SHOW NAVBAR ON MOBILE AND TABLET
+
+document.getElementById("click-btn").onclick = function () {
+  let check = document.getElementById("disable");
+  check.classList.toggle("visibility");
+};
